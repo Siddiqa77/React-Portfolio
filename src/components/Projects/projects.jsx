6b43086df402a45884b30510173a2da828047a02
Siddiqa Badar <147896187+Siddiqa77@users.noreply.github.com>
2024-10-18 22:36:38 +0500

    projects.jsx

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -10,42 +10,42 @@ const Projects = () => {
         <ProjectCard
           title="Wedding Website"
           description="This is a Wedding Planner website created by HTML, CSS and JS with some component libraries for better user experience and styling."
-          image="../../../public/images/project1.png"
+          image="/images/project1.png"
           demourl="https://wedding-planner-website-liard.vercel.app/"
           codeurl="#"
         />
         <ProjectCard
           title="Ecommerce Website"
           description="This is a ZULU Shoes Ecommerce website created by HTML, CSS and JS with some component libraries for better user experience and styling."
-          image="../../../public/images/project2.png"
+          image="/images/project2.png"
           demourl="https://internship-pakistan-task-4.vercel.app/"
           codeurl="#"
         />
         <ProjectCard
           title="Techforcepakistan "
           description="This is a Wedding Planner website created by HTML and CSS with some component libraries for better user experience and styling.."
-          image="../../../public/images/project3.png"
+          image="/images/project3.png"
           demourl="https://js-website-8cddd.web.app/"
           codeurl=""
         />
         <ProjectCard
           title="Event Website"
           description="A fully functional e-commerce store with a modern UI, built using Next.js and styled with Tailwind CSS."
-          image="../../../public/images/project4.png"
+          image="/images/project4.png"
           demourl="https://web4-ef5db.web.app/"
           codeurl="https://github.com/Siddiqa77/Portfolio/tree/main/Event%20website%20project"
         />
         <ProjectCard
           title=" NextJS Portfolio"
           description="A personal portfolio showcasing various web development projects. Developed using HTML, Next.js, and Tailwind CSS."
-          image="../../../public/images/project5.png"
+          image="/images/project5.png"
           demourl="https://siddiqaportfolio.vercel.app/"
           codeurl="https://github.com/Siddiqa77/My-Portfolio-Next.js"
         />
         <ProjectCard
           title="Image Gallery"
           description="A responsive Image Gallery with Large Image interface  With HTML and CSS by using some libraries."
-          image="public/images/project6.png"
+          image="/images/project6.png"
           demourl="https://code-alpha-image-gallery-rho.vercel.app/"
           codeurl="https://github.com/Siddiqa77/CodeAlpha_Image_Gallery"
         />

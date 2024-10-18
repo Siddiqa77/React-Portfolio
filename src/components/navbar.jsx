4fbdf1b65776243c8c4737f619c454ba4684e758
Siddiqa Badar <147896187+Siddiqa77@users.noreply.github.com>
2024-10-18 21:01:36 +0500

    navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,12 @@
+
+
 import React, { useState } from "react";
 import { RiCloseLine, RiMenu2Line } from "@remixicon/react";
-
 
 const Navbar = () => {
   const [menu, openMenu] = useState(false);
   const [showMenu, setShowmenu] = useState(true);
+
   return (
     <nav className="flex flex-wrap justify-between md:items-center text-white px-10 pt-6 md:px-20">
       <span className="text-2xl font-bold tracking-wide hover:animate-spin">Siddiqa Badar</span>
@@ -12,7 +14,7 @@ const Navbar = () => {
       <ul
         className={`${
           menu ? "block" : "hidden"
-        }     mx-24 p-y2 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl bg-opacity-30 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6`}
+        } mx-24 py-2 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl bg-opacity-30 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6`}
       >
         <a href="#About">
           <li className="text-md transition-all duration-300 p-1 md:p-0 mr-5 text-xl hover:bg-[#D105FF] hover:p-2 hover:rounded-2xl cursor-pointer">
@@ -35,6 +37,7 @@ const Navbar = () => {
           </li>
         </a>
       </ul>
+      
       {showMenu ? (
         <RiMenu2Line
           size={30}
@@ -48,10 +51,10 @@ const Navbar = () => {
         <RiCloseLine
           size={30}
           className="md:hidden absolute right-10 top-6 transition-all duration-300"
-          onClick={() => 
+          onClick={() => {
             openMenu(false);
             setShowmenu(true);
-          }
+          }}
         />
       )}
     </nav>
@@ -59,3 +62,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
